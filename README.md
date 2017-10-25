235f13f654805b38c754413555553fb7f08bcdef
Yash <yash592@gmail.com>
2017-10-25 14:58:32 -0700

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hangman-Game
+
+Silly Hangman game where user tries to guess a word.
+
+Technologies used: HTML, CSS Bootstrap, JS.
---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// Pseudocode
+
+// Setup html.
+
+// Make an array of words.
+
+// Set up number of guesses
+
+// Set up blank, hidden word.
+
+// Capture key presses and plug it into spaces.
+
+// if userword = arrayword, then win, score++
+
+// Move on to the next word.
+
+var words = [];
+
+
