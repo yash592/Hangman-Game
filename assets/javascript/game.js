1fd456a0b5d2b372353c8535e93ddfb90e9492ba
Yash <yash592@gmail.com>
2017-10-26 23:44:43 -0700

    Additional logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,111 @@
-// Pseudocode
 
-// Setup html.
 
-// Make an array of words.
 
-// Set up number of guesses
+var words = ["bached", "baches", "backed", "backup", "badger", "bagels", "bagful"];
+var score = 0;
+var guesses = 6;
+var losses = 0;
+var guessarray = [];
+var correctguesses = [];
+var wrongguesses =[];
 
-// Set up blank, hidden word.
 
-// Capture key presses and plug it into spaces.
 
-// if userword = arrayword, then win, score++
 
-// Move on to the next word.
+function pickaword () {
+	 pickedword = words[Math.floor(Math.random()*words.length)];
+};
 
-var words = [];
+pickaword();
+console.log(pickedword);
+
+function makeunderscorearray () {
+	for ( i = 0; i < pickedword.length; i++ ) {
+		guessarray.push("_");
+
+	}
+
+document.getElementById("secret").innerHTML = guessarray;	
+
+}
+
+makeunderscorearray();
+console.log(guessarray);
+
+
+
+function resetgame () {
+
+	guesses = 6;
+	word = pickaword();
+	newarray = makeunderscorearray();
+	wrongguesses = [];
+
+}
+
+
+
+document.onkeyup = function () {
+	var userGuess = event.key;
+	console.log(event.key);	
+
+	if (pickedword.indexOf(userGuess) > -1) {
+
+		correctguesses.push(userGuess);
+		guessarray[pickedword.indexOf(userGuess)] = userGuess;		
+		
+		score++;
+
+	}
+
+	else {
+
+	wrongguesses.push(userGuess);
+	document.getElementById("guessed").innerHTML = "Letters already guessed: " + wrongguesses;
+	
+	guesses--;
+	document.getElementById("remaining").innerHTML ="Number of guesses remaining: " + guesses;
+
+}
+
+if (guessarray.indexOf("_") < 0) {
+	alert("You win!");
+	document.getElementById("wins").innerHTML ="Wins: " + score;
+	resetgame();
+
+}
+
+if (guesses <= 0) {
+	alert("Game over!");
+	losses++;
+	document.getElementById("losses").innerHTML ="Losses: " + losses;
+	resetgame();
+	console.log(pickedword);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
 
 
